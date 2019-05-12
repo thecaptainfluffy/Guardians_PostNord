54fc59215d264fd2b8148b0f5f5f93e77112e59b
OskarForssman <Oskar.forssman@hotmail.com>
2019-05-12 09:36:36 +0200

    sms

--- app/src/main/java/com/example/trypackagemanager/guardians_postnord/MainActivity.java
+++ app/src/main/java/com/example/trypackagemanager/guardians_postnord/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.trypackagemanager.guardians_postnord;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -17,6 +19,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.common.api.GoogleApiActivity;
 
@@ -73,7 +77,34 @@ public class MainActivity extends AppCompatActivity
         System.out.println(name1 + " " + name2 + " " + name3 +" "+name4 +" "+ address1 +" "
                 + address2 +" " + address3 + " " +address4);
 
+
+
+
+
+        final Button button = (Button) findViewById(R.id.nej);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phonenumber="0734244419";
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("sms:"
+                        + phonenumber)));
+
+                String sms="Testing";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phonenumber));
+                intent.putExtra("sms_body", sms);
+                startActivity(intent);
+
+            }
+        });
     }
+
+
+
+
+
+
+
+
+
 
 
 
